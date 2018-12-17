0383d17278c427650c828f10821fe96f9bed59df
FujiiYuta <yutahand1@yahoo.co.jp>
2018-12-18 03:57:16 +0900

    small fix

--- watchAC_knn.py
+++ watchAC_knn.py
@@ -5,13 +5,15 @@ X = shake.data      # 説明変数
 Y = shake.target    # 目的変数
 '''
 import numpy as np
+
 # データの読み込み及び配列に変換
 f = np.loadtxt('shake1.csv', delimiter=',')
-f2 = np.loadtxt('wave1.csv', delimiter=',')
-'''
+g = np.loadtxt('wave1.csv', delimiter=',')
+
 # 配列の保存
 np.save('shake1.npy', f)
 np.save('wave1.npy', g)
-'''
-print(f)
+
+# print(f)
+# print(g)
 
