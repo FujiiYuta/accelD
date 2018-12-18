db88aee6a56fd69717d9fe45213d6c107cce62e6
FujiiYuta <yutahand1@yahoo.co.jp>
2018-12-18 16:44:25 +0900

    small fix

--- watchAC_knn.py
+++ watchAC_knn.py
@@ -7,8 +7,8 @@ Y = shake.target    # 目的変数
 import numpy as np
 
 # データの読み込み及び配列に変換
-f = np.loadtxt('shake1.csv', delimiter=',')
-g = np.loadtxt('wave1.csv', delimiter=',')
+f = np.loadtxt('shake1.csv', delimiter=',', dtype='int32')
+g = np.loadtxt('wave1.csv', delimiter=',', dtype='int32')
 
 # 配列の保存
 np.save('shake1.npy', f)
@@ -16,4 +16,28 @@ np.save('wave1.npy', g)
 
 # print(f)
 # print(g)
+# npyの表示はpythonのインタラクティブエディタで確認済
 
+# 0または1の配列を作成
+# 0 : shake
+# 1 : wave
+a = np.full((4, 1), 0, dtype='int32')
+b = np.full((4, 1), 1, dtype='int32')
+
+# 作成した配列とf,gを結合させる(0または1の配列を4行目に追加する)
+shake = np.hstack((f, a))
+wave = np.hstack((g, b))
+
+#shakeとwaveを一つのdatasetにまとめる
+dataset = shake
+dataset = np.append(dataset, wave, axis=0)
+# これってdataset = np.append(shake, waveと同義じゃないの？
+
+#datasetをランダムに並び替える
+np.random.shuffle(dataset)
+
+# trainとtestに分割(train_data = 7, test_data = 3)
+train = dataset[:7]
+test = dataset[7:]
+
+# dataとlabelに分割
