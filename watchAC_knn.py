a720301e7cd1388bb9566a19c1396b055f1d5895
FujiiYuta <yutahand1@yahoo.co.jp>
2019-01-04 12:57:14 +0900

    fix the python file

--- watchAC_knn.py
+++ watchAC_knn.py
@@ -21,10 +21,10 @@ np.save('wave1.npy', g)
 # 0または1の配列を作成
 # 0 : shake
 # 1 : wave
-a = np.full((4, 1), 0, dtype='int32')
-b = np.full((4, 1), 1, dtype='int32')
+a = np.full((522, 1), 0, dtype='int32')
+b = np.full((332, 1), 1, dtype='int32')
 
-# 作成した配列とf,gを結合させる(0または1の配列を4行目に追加する)
+# 作成した配列とf,gを結合させる(0または1のカラムを4行目に追加する)
 shake = np.hstack((f, a))
 wave = np.hstack((g, b))
 
@@ -36,9 +36,10 @@ dataset = np.append(dataset, wave, axis=0)
 #datasetをランダムに並び替える
 np.random.shuffle(dataset)
 
-# trainとtestに分割(train_data = 7, test_data = 3)
-train = dataset[:7]
-test = dataset[7:]
+# trainとtestに分割(train_data : test_data = 7 : 3)
+# つまり, (522+332)*7/10 = (about)598
+train = dataset[:588]
+test = dataset[598:]
 
 # dataとlabelに分割
 # [-1]は「最後の要素」という意味
@@ -46,20 +47,25 @@ test = dataset[7:]
 # 座標軸、ということらしい
 # 1は横方向(つまり二次元配列を縦線で割るということ
 train_data, train_label = np.split(train, [-1], axis=1)
-test_data, test_label = np.spilit(test, [-1], axis=1)
+test_data, test_label = np.split(test, [-1], axis=1)
 
 # datasetの保存
 
 x = (train_data, train_label, test_data, test_label)
 np.save('dataset.npy', x)
 
+#dataの数をそれぞれ出力
+print("train_data", "test_data")
+print(len(train_data), len(test_data))
 # 次回呼び出し
 # train_data, train_label, test_data, test_label = np.load('dataset.npy')
 
 # ここまででデータセットの準備は終了
 # やっと機械学習のフェーズへ
 # sklearnを用いて、knnで機械学習
+'''
 import matplotlib.pyplot as plt
 %matplotlib inline
 from matplotlib.colors import ListedColormap
 from sklearn import neighbors, datasets, metrics
+'''
