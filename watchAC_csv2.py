29e1e84817cb19df5722efd062bfdcbfe4dfdc8d
FujiiYuta <yutahand1@yahoo.co.jp>
2019-01-17 00:00:34 +0900

    do learning with knn

--- watchAC_csv2.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
- 
-# csvファイルのimportと一緒にカラムを設定                                                                                                                  
-shake = pd.read_csv('shake1.csv', names=['userAccelerate.x', 'userAccelerate.y', 'userAccelerate.z'])
-print(shake)
